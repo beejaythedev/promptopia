3eaf6b3aa03738451ea0586aa19995ea1bfc9d65
Odumosu Bolaji <odmusbolaji31@gmail.com>
2025-05-28 13:46:14 +0100

    Updated codebase by removing useSearchParams() to get Id for the post to edit

---
+++ app/update-prompt/[id]/page.js
@@ -0,0 +1,6 @@
+import EditPrompt from "../../../components/EditPrompt";
+
+export default function UpdatePrompt ({ params }) {
+   
+    return <EditPrompt promptId={params.id} />;
+}
--- components/EditPrompt.jsx
+++ components/EditPrompt.jsx
@@ -2,22 +2,23 @@
 
 import { useEffect, useState } from 'react'
 
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
-import Form from '../../components/Form'
+import Form from './Form'
 
-const EditPrompt = () => {
+const EditPrompt = ({ promptId }) => {
     const router = useRouter();
+  
 
     const [submitting, setSubmitting] = useState(false)
     const [post, setPost] = useState({ prompt: '', tag: '' })
-    const searchParams = useSearchParams();
-    const promptId = searchParams.get('id');
+  
 
     useEffect(() =>{
         const fetchPostDetails = async () =>{
             const response = await fetch(`/api/prompt/${promptId}`)
             const data = await response.json()
+           
             setPost({
                 prompt: data.prompt,
                 tag: data.tag
